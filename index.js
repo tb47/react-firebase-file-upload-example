8554207d51c19e1d8170db47c6c2b9bb914d5804
Thorsten Franz Brune <40182539+tb47@users.noreply.github.com>
2020-12-10 16:32:22 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Upload from './component/upload';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>file metaservice</h1>
+        <Upload />
+    </div>
+  );
+}
+
+export default App;
